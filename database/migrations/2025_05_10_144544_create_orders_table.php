9701da8bffe62a13ef500903e35e468da2df5966
Youssef Ashraf <youssef.ashraf.othman120@gmail.com>
2025-05-10 17:46:32 +0300

    updating orders table

--- database/migrations/2025_05_10_144544_create_orders_table.php
+++ database/migrations/2025_05_10_144544_create_orders_table.php
@@ -4,34 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-
-            //order information
-            $table->decimal('total_amount', 10, 2);
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index('orders_user_id_foreign');
+            $table->decimal('total_amount', 10);
             $table->enum('payment_method', ['cash_on_delivery', 'credit_card']);
             $table->string('status')->default('جاري التوصيل');
-
-            //client information
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->nullable();
             $table->string('phone');
             $table->string('city');
             $table->text('address');
-
-            //additional information 
             $table->text('admin_notes')->nullable();
-
+            $table->longText('content')->nullable();
             $table->timestamps();
+            $table->bigInteger('order_id');
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('orders');
