02ce9a53befd13fedd3f47d2e8033bdd80939478
Youssef Ashraf <youssef.ashraf.othman120@gmail.com>
2025-05-10 15:20:22 +0300

    Solving assets problem

--- storage/framework/views/822fa8a8b30f0197a3ab06192c75672d.php
+++ storage/framework/views/822fa8a8b30f0197a3ab06192c75672d.php
@@ -10,26 +10,24 @@
 	<title>JO - Store</title>
 
 	<!-- favicon -->
-	<link rel="shortcut icon" type="image/png" href="<?php echo e(asset('assets/img/favicon.png')); ?>">
-	<!-- google font -->
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="<?php echo e(secure_asset('assets/img/favicon.png')); ?>">
 	<!-- fontawesome -->
-	<link rel="stylesheet" href="<?php echo e(asset('assets/css/all.min.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(secure_asset('assets/css/all.min.css')); ?>">
 	<!-- bootstrap -->
-	<link rel="stylesheet" href="<?php echo e(asset('assets/bootstrap/css/bootstrap.min.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(secure_asset('assets/bootstrap/css/bootstrap.min.css')); ?>">
 	<!-- owl carousel -->
-	<link rel="stylesheet" href="<?php echo e(asset('assets/css/owl.carousel.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(secure_asset('assets/css/owl.carousel.css')); ?>">
 	<!-- magnific popup -->
-	<link rel="stylesheet" href="<?php echo e(asset('assets/css/magnific-popup.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(secure_asset('assets/css/magnific-popup.css')); ?>">
 	<!-- animate css -->
-	<link rel="stylesheet" href="<?php echo e(asset('assets/css/animate.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(secure_asset('assets/css/animate.css')); ?>">
 	<!-- mean menu css -->
-	<link rel="stylesheet" href="<?php echo e(asset('assets/css/meanmenu.min.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(secure_asset('assets/css/meanmenu.min.css')); ?>">
 	<!-- main style -->
-	<link rel="stylesheet" href="<?php echo e(asset('assets/css/main.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(secure_asset('assets/css/main.css')); ?>">
 	<!-- responsive -->
-	<link rel="stylesheet" href="<?php echo e(asset('assets/css/responsive.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(secure_asset('assets/css/responsive.css')); ?>">
+
 
 </head>
 <body>
