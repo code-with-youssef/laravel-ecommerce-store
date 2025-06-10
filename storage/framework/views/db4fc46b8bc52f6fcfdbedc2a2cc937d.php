d987aa93edca68cd0f90ba09a8bb794732f2a343
Youssef Ashraf <youssef.ashraf.othman120@gmail.com>
2025-06-10 23:33:22 +0300

    Updating Files

--- storage/framework/views/db4fc46b8bc52f6fcfdbedc2a2cc937d.php
+++
@@ -1,4 +0,0 @@
-<li class="<?php echo e(request()->routeIs($route) ? 'current-list-item' : ''); ?>">
-    <a href="<?php echo e(route($route)); ?>"><?php echo e($label); ?></a>
-</li>
-<?php /**PATH F:\E-commerce app\E-commerce-app\resources\views/components/nav-link.blade.php ENDPATH**/ ?>
